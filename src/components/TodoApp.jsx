165b41d2b3c25f7aff24a97918cbc401ba2afd65
EdsonJSF <edsonjsf1990@gmail.com>
2023-12-27 09:17:41 -0400

    Feat: RTK Query implementatin

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+export const TodoApp = () => {
+  return (
+    <>
+      <h1>TodoApp - RTK Query</h1>
+      <hr />
+      <h4>Loading ...</h4>
+
+      <pre>...</pre>
+
+      <button>Next todo</button>
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,15 @@ import { store } from "./store";
 import "./main.css";
 
 // import { Counter } from "./components/Counter";
-import { PokemonApp } from "./components/pokemonApp";
+// import { PokemonApp } from "./components/pokemonApp";
+import { TodoApp } from "./components/TodoApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <Counter /> */}
-      <PokemonApp/>
+      {/* <PokemonApp/> */}
+      <TodoApp/>
     </Provider>
   </React.StrictMode>
 );
