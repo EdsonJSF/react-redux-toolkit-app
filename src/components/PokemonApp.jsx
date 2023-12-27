2d07c31561472265ac8905e6dcc47803f22589e4
EdsonJSF <edsonjsf1990@gmail.com>
2023-12-27 08:58:27 -0400

    Feat: Show pokemons

--- src/components/PokemonApp.jsx
+++ src/components/PokemonApp.jsx
@@ -1,9 +1,12 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import { getPokemons } from "../store/slices";
 
 export const PokemonApp = () => {
+  const { page, pokemons, isLoading } = useSelector(
+    (state) => state.pokemonReducer
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,11 +18,25 @@ export const PokemonApp = () => {
       <h1>PokemonApp</h1>
       <hr />
 
-      <ul>
-        <li>Hola</li>
-        <li>Hola</li>
-        <li>Hola</li>
-      </ul>
+      {isLoading ? (
+        <span>Loading ...</span>
+      ) : (
+        <>
+          <ul>
+            {pokemons.map(({ name }) => (
+              <li key={name}>{name}</li>
+            ))}
+          </ul>
+
+          <button
+            disabled={page < 2}
+            onClick={() => dispatch(getPokemons(page - 2))}
+          >
+            Back
+          </button>
+          <button onClick={() => dispatch(getPokemons(page))}>Next</button>
+        </>
+      )}
     </>
   );
 };
